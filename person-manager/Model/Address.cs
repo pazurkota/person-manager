d69ddf2e4b2777f941a5b4cbf260c479fbe3904f
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-02-26 14:34:33 +0100

    Added Person.cs and Adress.cs

---
+++ person-manager/Model/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace person_manager.Model;
+
+public class Address
+{
+    public string City;
+    public string Street;
+    public int HomeNumber;
+    public int? ApartmentNumber; 
+
+    public override string ToString()
+    {
+        return $"{City}, {Street} st. ({HomeNumber}{(ApartmentNumber.HasValue ? $"/{ApartmentNumber}" : "")}";
+    }
+}
---
+++ person-manager/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace person_manager.Model;
+
+public class Person
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public int PhoneNumber { get; set; }
+    public Address Address { get; set; }
+
+    public override string ToString()
+    {
+        return $"({ID}) {Name} {Surname}, Age: {Age}, PhoneNumber: {PhoneNumber}\nAddress: {Address}";
+    }
+}
