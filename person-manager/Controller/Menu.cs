ae4a94d552e4da00805ce0bd5652d8243d60428f
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-02-27 20:11:15 +0100

    Added Menu.cs

---
+++ person-manager/Controller/Menu.cs
@@ -0,0 +1,17 @@
+﻿namespace person_manager.Controller;
+
+public class Menu
+{
+    private string showMenu()
+    {
+        string str = "Person Manager menu";
+
+        str += "\n1. About";
+        str += "\n2. Show full list";
+        str += "\n3. Add new person";
+        str += "\n4. Remove person (via ID)";
+        str += "\n0. Exit program";
+
+        return str;
+    }
+}
